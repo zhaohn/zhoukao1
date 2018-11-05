7d0a10cddc998531a93c41382c972f8f47c73b4d
zhaohaonan <2356466393@qq.com>
2018-11-05 09:12:22 +0800

    外部文件的读取引入

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,14 @@
  * @Author: zhaohaonan 
  * @Date: 2018-11-05 08:46:17 
  * @Last Modified by: zhaohaonan
- * @Last Modified time: 2018-11-05 08:57:10
+ * @Last Modified time: 2018-11-05 09:11:55
  */
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var clean = require('gulp-clean-css');
 var server = require('gulp-webserver');
+var fs = require('fs');
+var path = require('path');
 gulp.task('devSass', function() {
     return gulp.src('./src/scss/*.scss')
         .pipe(sass())
@@ -23,7 +25,14 @@ gulp.task('devServer', function() {
             port: 8080,
             host: '169.254.163.44',
             middleware: function(req, res, next) {
-
+                var pathname = require('url').parse(req.url).pathname;
+                if (pathname === '/favicon.ico') {
+                    res.end();
+                } else if (pathname === '/') {
+                    res.end(fs.readFileSync(path.join(__dirname, 'src', 'index.html')));
+                } else {
+                    res.end(fs.readFileSync(path.join(__dirname, 'src', pathname)));
+                }
             }
         }))
 })
